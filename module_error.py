1314676168c367430768b90e6f6d706dd1dd097e
prompt-laser <gillydorf@gmail.com>
2018-08-08 14:53:09 -0500

    Create module_error.py

---
+++ module_error.py
@@ -0,0 +1,11 @@
+def plugin_about():
+	return "Error handling and error responses"
+
+def plugin_trigger():
+	return "error"
+
+def plugin_help():
+	return "Error handling and error responses"
+
+def plugin_main(args):
+	return "Error"
