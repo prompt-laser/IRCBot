4f925075fb50cc3ffdb60aecdb48f8765ce3d3e7
prompt-laser <gillydorf@gmail.com>
2018-08-08 14:51:12 -0500

    Errors and OS
    Updated error handling to call new error module. Changed module string for change to linux.

--- irc.py
+++ irc.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3.6
+
 import glob
 import socket
 import importlib
@@ -28,13 +30,13 @@ def plugin_get_about(name):
 	
 def load_plugins(*args, **kwargs):
 	print("Finding plugins...")
-	mods = glob.glob("c:\\python\\module_*.py")
+	mods = glob.glob("module_*.py")
 	modList = []
 
 	print("Loading plugins...")
 	i = 0
 	for mod in mods:
-		mods[i] = mods[i].replace("c:\\python\\module_", "")
+		mods[i] = mods[i].replace("module_", "")
 		mods[i] = mods[i].replace(".py", "")
 		if(kwargs.get('reload')):
 			modList.append(Plugin(mods[i], reload=True))
@@ -51,7 +53,7 @@ class Plugin(object):
 	about = ""
 	help = ""
 	
-	def __init__(self, name):
+	def __init__(self, name, *args, **kwargs):
 		self.name = name
 		if(kwargs.get('reload')):
 			reload_plugin(name)
@@ -71,11 +73,11 @@ modList = load_plugins()
 HOSTNAME = 'chat.freenode.net'
 PORT = 6667
 BUFFER_SIZE = 1024
-COMMAND_CHAR = command_character_string 		# "@"
-CHANNEL = channel_string				# "#freenode"
-USER = user_string					# "guest"
-NICK = nick_string					# "guest"
-REAL_NAME = real_name_string				# "this is my real name"
+COMMAND_CHAR = command_char_string				#"@"
+CHANNEL = channel_name_string					#"myChannel"
+USER = username_string							#"myUsername"
+NICK = nick_string								#"myNick"
+REAL_NAME = real_name_string					#"Mr. Smith"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOSTNAME, PORT))
@@ -103,33 +105,38 @@ while(True):
 			s.send(bytes("PONG " + message.split()[1], "ASCII"))
 		elif(message.find("PRIVMSG") != -1):
 			segments = message.split(":")
-			if(segments[2][0] == COMMAND_CHAR):
-				command = segments[2].split()[0]
-				if(command.find("\\r\\n'") != -1):
-					command = command.replace("\\r\\n'", "")
-				arguments = segments[2].replace(command, "")
-				command = command.replace(COMMAND_CHAR, "")
-				arguments = arguments.replace("\\r\\n'", "")
-				arguments = arguments.strip()
-				if arguments is None:
-					arguments = ""
+			if(segments[2] != ""):
+				if(segments[2][0] == COMMAND_CHAR):
+					command = segments[2].split()[0]
+					if(command.find("\\r\\n'") != -1):
+						command = command.replace("\\r\\n'", "")
+					arguments = segments[2].replace(command, "")
+					command = command.replace(COMMAND_CHAR, "")
+					arguments = arguments.replace("\\r\\n'", "")
+					arguments = arguments.strip()
+					if arguments is None:
+						arguments = ""
 				
-				if(command.lower().find("help") != -1):
-					s.send(bytes("PRIVMSG " + CHANNEL + " :Here's a list of my loaded plugins\r\n", "ASCII"))
-					for mod in modList:
-						s.send(bytes("PRIVMSG " + CHANNEL + " :" + mod.name + " - " + mod.about + "\r\n", "ASCII"))
-				elif(command.lower().find("reload") != -1):
-					modList = load_plugins(reload=True)
-				else:
-					for mod in modList:
-						if(mod.trigger == command.lower()):
-							if(arguments == "help"):
-								s.send(bytes("PRIVMSG " + CHANNEL + " :" + mod.help + "\r\n", "ASCII"))
-							elif(arguments == "about"):
-								s.send(bytes("PRIVMSG " + CHANNEL + " :" + mod.about + "\r\n", "ASCII"))
-							else:
-								print("Trying to execute command: " + command + " with arguments: " + arguments)
-								try:
-									s.send(bytes("PRIVMSG " + CHANNEL + " :" + call_plugin(mod.name, arguments) + "\r\n", "ASCII"))
-								except Exception as e:
-									s.send(bytes("PRIVMSG " + CHANNEL + " :Well, this is embarassing...\r\n", "ASCII"))
+					if(command.lower().find("help") != -1):
+						s.send(bytes("PRIVMSG " + CHANNEL + " :Here's a list of my loaded plugins\r\n", "ASCII"))
+						for mod in modList:
+							s.send(bytes("PRIVMSG " + CHANNEL + " :" + mod.name + " - " + mod.about + "\r\n", "ASCII"))
+					elif(command.lower().find("reload") != -1):
+						modList = load_plugins(reload=True)
+					else:
+						for mod in modList:
+							if(mod.trigger == command.lower()):
+								if(arguments == "help"):
+									s.send(bytes("PRIVMSG " + CHANNEL + " :" + mod.help + "\r\n", "ASCII"))
+								elif(arguments == "about"):
+									s.send(bytes("PRIVMSG " + CHANNEL + " :" + mod.about + "\r\n", "ASCII"))
+								else:
+									print("Trying to execute command: " + command + " with arguments: " + arguments)
+									pluginResponse = call_plugin(mod.name, arguments)
+									if(pluginResponse.split(" ")[0].lower() != "error"):
+										try:
+											s.send(bytes("PRIVMSG " + CHANNEL + " :" + pluginResponse + "\r\n", "ASCII"))
+										except Exception as e:
+											s.send(bytes("PRIVMSG " + CHANNEL + " :" + call_plugin("error", "insert Error getting bytes from string from command " + mod.name + ": " + pluginResponse) + "\r\n", "ASCII"))
+									else:
+										s.send(bytes("PRIVMSG " + CHANNEL + " :" + call_plugin("error", "insert " + pluginResponse) + "\r\n", "ASCII"))
